a7dac291b57cf2c9c410249a935acef1912a4ff8
MartynasZiliusPXL <12401644@student.pxl.be>
2024-10-10 15:54:27 +0100

    Alles afgewerkt
    Alles is afgewerkt

---
+++ GitTaak1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitTaak1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Film1_Click(object sender, MouseButtonEventArgs e)
+        {
+            titelLabel.Content = "Dune: Part Two";
+            beschrijvingTextBlock.Text = "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.";
+        }
+
+        private void Film2_Click(object sender, MouseButtonEventArgs e)
+        {
+            titelLabel.Content = "Furiosa: A Mad Max Saga";
+            beschrijvingTextBlock.Text = "The origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.";
+        }
+
+        private void Film3_Click(object sender, MouseButtonEventArgs e)
+        {
+            titelLabel.Content = "The Substance";
+            beschrijvingTextBlock.Text = "A fading celebrity decides to use a black-market drug, a cell-replicating substance that temporarily creates a younger, better version of herself.";
+        }
+    }
+}
